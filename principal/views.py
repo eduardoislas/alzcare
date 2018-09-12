2f0402f38eb90e16b7a66bea9a179d4f25b2527e
Eduardo Islas <eduardo@1guillermo-pc.itson.edu.mx>
2018-09-11 19:42:54 -0700

    Integración del template base

--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    return render(request,"principal/home.html") 
+
+    
+def login (request):
+    return render(request,"principal/login.html") 
