616747fe66fe503eee1ea1883e2d870f428ac05d
damian <ocampo.damian@hotmail.com>
2018-09-15 13:55:53 -0700

    Tablas

--- principal/views.py
+++ principal/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Instrument
 
 # Create your views here.
 
 def home (request):
     return render(request,"principal/home.html") 
+
+def instrumentos (request):
+    instruments = Instrument.objects.all()
+    return render(request,"principal/instrumentos.html",{'instruments':instruments})
 
     
 def login (request):
